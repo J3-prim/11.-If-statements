b73226bb958270faca161d8ceae070d4820c513a
Justin Prim <justinprim140969@gmail.com>
2024-01-10 14:57:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.If_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true; 
+
+            if (isMale) 
+            {
+                Console.WriteLine("You are male"); 
+            }
+            else
+            {
+                Console.WriteLine("You are female");
+            }
+
+            bool isTall = false; 
+            if (isTall&&isMale) 
+            {
+                Console.WriteLine("Both male and tall");
+            }
+            else if (!isTall&&isMale)
+            {
+                Console.WriteLine("short male");
+            } 
+            else
+            {
+                Console.WriteLine("either not tall or female"); 
+
+            }
+            Console.Read(); 
+        }
+    }
+}
